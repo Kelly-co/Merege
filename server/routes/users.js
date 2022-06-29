d0624d1f8a89cc6a16758ffe8794616a8176a808
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-06-29 15:57:00 +0100

    updated main components and packages

---
+++ server/routes/users.js
@@ -0,0 +1,15 @@
+// const usersRouter = require('express').Router();
+// const User = require('../models/user');
+
+// usersRouter.get('/users', (req, res) => {
+//     User.findMany()
+//         .then((results) => {
+//             res.json(results);
+//         })
+//         .catch((err) => {
+//             console.error(err);
+//             res.status(500).send('Error retrieving users from database');
+//         });
+// });
+
+// module.exports = usersRouter;
