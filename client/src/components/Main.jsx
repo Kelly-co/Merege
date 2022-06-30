1580dc11701e1bab326c222e1ffca5df4765b9da
Juan Giménez <juanp.gi2485@gmail.com>
2022-06-30 17:08:31 +0100

    fix

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import ProjectList from "../components/Dashboard/ProjectList";
-<<<<<<< HEAD
 import Homepage from "./Homepage/Homepage";
-import Form from "./Form/Form";
-=======
 import Form from "../components/Form/Form";
->>>>>>> 93057dc4ce2553f09570c8209cc8e21f9ef76478
 
 function Main() {
   return (
