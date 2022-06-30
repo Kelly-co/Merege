67e8b56d64843a15177495f16ca27906c87dd090
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-06-30 15:52:36 +0100

    removed node_modules

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Form from "./components/Form/Form.jsx";
+import Homepage from "./components/Homepage/Homepage.jsx";
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
