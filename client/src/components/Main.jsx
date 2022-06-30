93057dc4ce2553f09570c8209cc8e21f9ef76478
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-06-30 16:08:57 +0100

    added form route

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import ProjectList from "../components/Dashboard/ProjectList";
+import Form from "../components/Form/Form";
 
 function Main() {
   return (
     <div className="main">
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="/form" element={<Form />} />
           <Route path="/project" element={<ProjectList />} />
         </Route>
       </Routes>
