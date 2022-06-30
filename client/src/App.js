075c023643d5c913dbc00b4e7859a6643e9945d3
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-06-30 15:02:38 +0100

    Changed the router in index

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import ProjectsContextProvider from "./contexts/ProjectsContext";
 import Main from "./components/Main";
-
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
-
-
   return (
     <div className="App">
       <ProjectsContextProvider>
         <Router>
-          <Main/>
+          <NavBar />
+          <Main />
         </Router>
       </ProjectsContextProvider>
-    </div >
+    </div>
   );
 }
 
