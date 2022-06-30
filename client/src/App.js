a3711ad230bb831b590eb66aca102575dc8a6efc
patriciasaraiva <patricia-saraiva_student2022@wilder.school>
2022-06-30 15:04:18 +0100

    modules

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Form from "./components/Form/Form.jsx";
+import Homepage from "./components/Homepage/Homepage.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Homepage />
       <Form />
     </div>
   );
