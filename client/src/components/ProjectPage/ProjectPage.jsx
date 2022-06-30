b49af3499117971b442cb96c2ce49e04a6fe395c
Juan Giménez <juanp.gi2485@gmail.com>
2022-06-30 17:48:25 +0100

    proj

--- client/src/components/ProjectPage/ProjectPage.jsx
+++ client/src/components/ProjectPage/ProjectPage.jsx
@@ -1,10 +1,19 @@
 import "./ProjectPage.css";
 import React from "react";
+import ProjectImg from "../../assets/images/LyonFilter.png";
+
 
 const ProjectPage = () => {
-  return <div>
-    
-  </div>;
+  return (
+    <div className="project-page">
+      <div style={{ backgroundImage: `url(${ProjectImg})`, backgroundRepeat: "no-repeat", backgroundSize:"cover" }}>
+        
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
 };
 
 export default ProjectPage;
