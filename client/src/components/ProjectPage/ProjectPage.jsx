3f2579e00d8befa2ffa859589126ccd8120fade5
Tiago Jacinto <53512597+TiagoJacinto92@users.noreply.github.com>
2022-06-30 01:53:46 +0100

    Started project page

--- client/src/components/ProjectPage/ProjectPage.jsx
+++ client/src/components/ProjectPage/ProjectPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const ProjectPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Project Page</h1>
+      <h2>Project Name</h2>
+      <p>Project Description</p>
+      <h5>Project Languages</h5>
+      <p>Project Collaborators</p>
+      <h5>Add Collaborators</h5>
+      <h5>Project Github Repository</h5>
+      <p>Project Trello Link</p>
+      <h5>Project keywords</h5>
+      <p>Project start and end date</p>
+    </div>
+  );
 };
 
 export default ProjectPage;
